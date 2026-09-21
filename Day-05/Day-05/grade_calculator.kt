5e44ce224e8f904e4a3153313789acbc01a57232
Aziz-81 <azizlama81@gmail.com>
2026-09-22 00:01:38 +0600

    grade_calculator.kt
    My learning on day-5 is also grade_calculator.kt

--- Day-05/Day-05/grade_calculator.kt
+++ Day-05/Day-05/grade_calculator.kt
@@ -1,1 +1,19 @@
+fun getGrade(marks: Int): String {
+    return when (marks) {
+        in 80..100 -> "A+"
+        in 70..79  -> "A"
+        in 60..69  -> "A-"
+        in 50..59  -> "B"
+        in 33..49  -> "C"
+        in 0..32   -> "F"
+        else       -> "Invalid"
+    }
+}
 
+fun main() {
+    println("85 → ${getGrade(85)}")
+    println("72 → ${getGrade(72)}")
+    println("45 → ${getGrade(45)}")
+    println("25 → ${getGrade(25)}")
+    println("110 → ${getGrade(110)}")
+}
