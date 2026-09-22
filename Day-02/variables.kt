390c01014bcf49ebe5aaabd5bc09ca2b8690105f
Aziz-81 <azizlama81@gmail.com>
2026-09-22 22:01:55 +0600

    Create variables.kt
    Today is my second day with Kotlin

---
+++ Day-02/variables.kt
@@ -0,0 +1,14 @@
+// Day 02: Variables and Data Types
+fun main() {
+    val name: String = "আজিজুল ইসলাম"
+    val age: Int = 31
+    val education: String = "আরবীতে মাস্টার্স"
+    val grade: Double = 4.8
+    val dream: String = "প্রফেশনাল এন্ড্রয়েড ডেভেলপার হওয়া"
+
+    println("নাম: $name")
+    println("বয়স: $age")
+    println("শিক্ষা: $education")
+    println("গ্রেড: $grade")
+    println("স্বপ্ন: $dream")
+}
